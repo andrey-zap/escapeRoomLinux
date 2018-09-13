045a9a9c73fb7287e22eaaec5f5e5d6a9b87a22b
andrey-zap <andrey.zaplavnov@develeap.com>
2018-09-13 12:44:13 +0000

    added initial script and modifications

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,9 @@
 FROM ubuntu:18.04
-RUN useradd -ms /bin/bash dude
-RUN ["apt-get", "update"]
-RUN ["apt-get", "install", "-y", "vim"]
-RUN ["apt-get", "install", "-y", "sudo"]
-WORKDIR /home/dude/
+RUN useradd -ms /bin/bash escape
+WORKDIR /home/escape/
 COPY escapeRoom escapeRoom
-
-RUN echo "root:escape" | chpasswd
-RUN echo "dude:dude" | chpasswd
-RUN sudo adduser dude sudo
-RUN ["chmod", "-R", "ago-w" ,"escapeRoom"]
-USER dude
+COPY initEscapeRoom.sh .
+RUN ["initEscapeRoom.sh"]
+USER escape
 ENTRYPOINT ["bash"]
 
--- initEscapeRoom.sh
+++ initEscapeRoom.sh
@@ -5,6 +5,7 @@ echo "escape:escape" | chpasswd
 apt-get update
 apt-get install -y vim
 apt-get install -y sudo
+apt-get install -y cowsay
 sudo adduser escape sudo
 
 echo "Preparation process COMPLETED"
@@ -15,20 +16,24 @@ echo "Preparation process COMPLETED"
 
 chmod -R agou-w ~/escapeRoom/room_1/
 
-#room2 preparation
+#room2 preparation , pass to readme : whatsupman
 
 chmod -R agou-w ~/escapeRoom/room_2/
-//edit readme
+
 //create white_rabbit user with id 521
+useradd -M -s /bin/false white_rabbit
+usermod -u 521 white_rabbit
+
+#room3 prep. pass to readme : 472
 
 
+chmod -R agou-w ~/escapeRoom/room_3/
 
-#room3
+chmod -R agou-x ~/escapeRoom/room_3/creature.sh
 
+//pass for treasure box :1195723
 
-
-
-
+#room4 prep pass to readme: 
 
 
 
