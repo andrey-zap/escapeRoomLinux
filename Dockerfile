57f016a6b87062c80da6b6277cebd8b8cb9c7267
andrey-zap <andrey.zaplavnov@develeap.com>
2018-09-13 14:25:27 +0000

    cowsay doesnt work

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM ubuntu:18.04
 RUN useradd -ms /bin/bash escape
-WORKDIR /home/escape/
+WORKDIR /home/escape
 COPY escapeRoom escapeRoom
 COPY initEscapeRoom.sh .
-RUN ["initEscapeRoom.sh"]
+RUN ./initEscapeRoom.sh
 USER escape
+
 ENTRYPOINT ["bash"]
 
--- initEscapeRoom.sh
+++ initEscapeRoom.sh
@@ -1,48 +1,36 @@
-#!/bin/bash
+#!/bin/sh
 #preparation tasks:
 echo "root:escaperoom" | chpasswd
 echo "escape:escape" | chpasswd
 apt-get update
-apt-get install -y vim
-apt-get install -y sudo
-apt-get install -y cowsay
+apt-get install -y apt-utils vim sudo cowsay
 sudo adduser escape sudo
 
 echo "Preparation process COMPLETED"
 
 #room1 preparation
-// ls -lah sort all files and get phrase from first letters of each one
-// read permissions to all only
+# ls -lah sort all files and get phrase from first letters of each one
+# read permissions to all only
 
-chmod -R agou-w ~/escapeRoom/room_1/
+chmod -R agou-w escapeRoom/room_1
 
 #room2 preparation , pass to readme : whatsupman
 
-chmod -R agou-w ~/escapeRoom/room_2/
+chmod -R agou-w escapeRoom/room_2/
 
-//create white_rabbit user with id 521
+#create white_rabbit user with id 521
 useradd -M -s /bin/false white_rabbit
 usermod -u 521 white_rabbit
 
 #room3 prep. pass to readme : 472
 
 
-chmod -R agou-w ~/escapeRoom/room_3/
+chmod -R agou-w escapeRoom/room_3/
 
-chmod -R agou-x ~/escapeRoom/room_3/creature.sh
+chmod -R agou-x escapeRoom/room_3/creature.sh
 
-//pass for treasure box :1195723
+#pass for treasure box :1195723
 
-#room4 prep pass to readme: 
+#room4 prep pass to readme: linuxIsFun
 
-
-
-
-
-
-
-
-
-
-
-Print every 5th line starting with the second
+chmod -R agou-w escapeRoom/room_4/
