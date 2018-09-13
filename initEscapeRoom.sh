86e89a04b93169b4775496e309f68e487b5ec6a6
andrey-zap <andrey.zaplavnov@develeap.com>
2018-09-13 09:52:34 +0000

    fgfg

---
+++ initEscapeRoom.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#preparation tasks:
+echo "root:escaperoom" | chpasswd
+echo "escape:escape" | chpasswd
+apt-get update
+apt-get install -y vim
+apt-get install -y sudo
+sudo adduser escape sudo
+
+echo "Preparation process COMPLETED"
+
+#room1
+// ls -lah sort all files and get phrase from first letters of each one
+// read permissions to all only
+
+chmod -R agou-w ~/escapeRoom/room_1/
+
+#room2
+
+//edit readme
+//create white_rabbit user with id 521
+
+
+
+#room3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Print every 5th line starting with the second
