ab4617ded994a33ea77af718a37514bccd902192
andrey-zap <andrey.zaplavnov@develeap.com>
2018-09-13 09:26:05 +0000

    modify rooms

---
+++ escapeRoom/room_3/creature.sh
@@ -0,0 +1,5 @@
+VimCrypt~02!
+ÓŠT{löý|u«ßjÎJÿqØWÈ@‰"ÿy×n‰Áð?ÙÏ\ŽL¶BÌÝ[„Fœ?×‰V‚@ù?Õ{Æ¯G·?¾È^Á@ãqGÐã˜ïaŸ(N*cîõ„™[SšÖ­|eSOŸÏô÷xV“ÛOßé«?(×E¤ÖZ× §l7ª~¶Ñ_5$‚Ïší#=ŸG)¤ö2HCPÇdâÂº|Ñq
+y9°iÇÉœ+´Î®LœÌ'àN[ZG
+	V½…ü•-ã¥¦IÐ «VV|öF[3îÌþ
+.÷ëZƒZ›Ú2ð¤zLÛõQË*yÍ‚ 2ÎÕ:úàžÛ(¯<œ7~	n…ÆGiCR Ù:dŒn›Ï~º5€š‰uð¬O3´^ð;<š5.žl€ÞèVÈ®å\Hí”É¢$ð}iöñ‚ÊéÒs
